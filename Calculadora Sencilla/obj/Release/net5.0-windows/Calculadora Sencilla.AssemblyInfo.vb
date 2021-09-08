5488a1703f7d2555cf96a831c254447a3c3174f8
CarlosEstrad <carlosestradavalle@outlook.com>
2021-09-08 17:01:23 -0500

    actualizar compilacion

--- Calculadora Sencilla/obj/Release/net5.0-windows/Calculadora Sencilla.AssemblyInfo.vb
+++ Calculadora Sencilla/obj/Release/net5.0-windows/Calculadora Sencilla.AssemblyInfo.vb
@@ -15,11 +15,11 @@ Imports System
 Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora Sencilla"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Release"),  _
- Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.1"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.2"),  _
+ Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.5"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.5"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("Calculadora Sencilla"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("Calculadora Sencilla"),  _
- Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.1"),  _
+ Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.5"),  _
  Assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0"),  _
  Assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")> 
 
